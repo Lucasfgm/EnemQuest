be8b853eea2b0090313abdd5494cdf5bb31b57d3
Lucas Flávio <lucasfgm00@gmail.com>
2023-12-02 16:25:30 -0300

    index

--- Data/Questao.cs
+++ Data/Questao.cs
@@ -5,7 +5,15 @@ public class Questao
         public int Id { get; set; }
         public string QuestaoTitulo {get; set;} = string.Empty;
 
-        public IEnumerable<string> Opcoes {get; set;} = new List<string>();
+        public string Opcao1 {get; set;} = string.Empty;
+
+        public string Opcao2 {get; set;} = string.Empty;
+
+        public string Opcao3 {get; set;} = string.Empty;
+
+        public string Opcao4 {get; set;} = string.Empty;
+
+        public string Opcao5 {get; set;} = string.Empty;
 
         public string Resposta {get; set;} = string.Empty;
 
